fe6050d42d30f2660570d0838f369c955d6d6143
Jaywood <Jaywood@yam.com>
2020-06-07 19:20:41 -0500

    Google books app

---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const DBUSER = process.env.MONGODB_USER;
+const DBPASS = process.env.MONGODB_PASS;
+
+// Configure body parsing for AJAX requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb+srv://"+DBUSER+":"+DBPASS+"@jhdevcluster-wejvn.mongodb.net/google-book?retryWrites=true&w=majority",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
